b5ac5701427b5a0d3f7204ad3466c11ae9cb7697
Hackipw <91828495+Hackipw@users.noreply.github.com>
2022-08-14 17:27:26 +0530

    Create vgaclub.py

---
+++ vgaclub.py
@@ -0,0 +1,12 @@
+apt update -y
+pkg install git -y
+pkg install python -y 
+pkg install figlet -y
+apt install ruby -y
+gem install lolcat
+pkg install figlet
+pkg install ruby
+gem install lolcat
+wget -q https://raw.githubusercontent.com/Hackipw/vga/main/vga.py
+clear
+python vga.py
